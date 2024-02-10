ffb2de82bf6555f2f01c53a54e172cf218b05eea
Thomas Fritz <coldelectrons@users.noreply.github.com>
2024-02-10 08:42:17 -0500

    revising...

--- system/hardware/cuda.nix
+++ system/hardware/cuda.nix
@@ -2,14 +2,27 @@
 
 {
   # OpenGL
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     pciutils
     cudatoolkit
-    cudnn
-    cutensor
   ];
 }

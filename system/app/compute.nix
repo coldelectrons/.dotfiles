936f42c18d01f573c8940acbda148d2d8058f8fa
Thomas Fritz <coldelectrons@users.noreply.github.com>
2024-02-10 08:46:03 -0500

    revising...

--- system/app/compute.nix
+++ system/app/compute.nix
@@ -12,5 +12,5 @@
   ];
   nix.settings.trusted-substituters = ["https://ai.cachix.org"];
   nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ nvidia ];
+  #boot.extraModulePackages = with config.boot.kernelPackages; [ nvidia ];
 }

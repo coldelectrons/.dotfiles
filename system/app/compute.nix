fa0611a3af3e1fff99754763582a801f41b9f2b0
Thomas Fritz <coldelectrons@users.noreply.github.com>
2024-02-10 08:17:47 -0500

    revising...

---
+++ system/app/compute.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    cudatoolkit
+    cudnn
+    cutensor
+    #python311Packages.torchWithCuda
+    #python311Packages.transformers
+    llama-cpp ollama
+    colmapWithCuda
+  ];
+  nix.settings.trusted-substituters = ["https://ai.cachix.org"];
+  nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ nvidia ];
+}

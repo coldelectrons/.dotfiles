6e366352da572b032ebbf329e0a28d21bbd21fa9
thomas <frithomas@gmail.com>
2024-02-17 15:16:15 -0500

    knolling

---
+++ user/app/cadcam/eda.nix
@@ -0,0 +1,15 @@
+{ pkgs, userSettings, ... }:
+
+{
+  home.packages = with pkgs; [
+    lepton-eda
+    pcb
+    librepcb
+    pcb2gcode
+    gerbv
+    #pcb-rnd??
+    kicad
+    kicadAddons.kikit
+  ];
+
+}

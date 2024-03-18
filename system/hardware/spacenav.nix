03e9fe94450cf02d989ab57dc9ddfbb1fa59f992
coldelectrons <frithomas@gmail.com>
2024-03-17 20:51:02 -0400

    added my own systemd unit for spacenav

--- system/hardware/spacenav.nix
+++ system/hardware/spacenav.nix
@@ -8,5 +8,15 @@
     usbview
   ];
 
+  # currently, does nothing because the nixpkg makes the systemd
+  # unit depend on graphical.target, which doesn't exist for users
   hardware.spacenavd.enable = true;
+
+  systemd.user.services.spacenavd = {
+    description = "Daemon for the Spacenavigator 6DOF mice by 3Dconnexion";
+    wantedBy = ["graphical-session.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.spacenavd}/bin/spacenavd -d -l syslog";
+    };
+  };
 }
